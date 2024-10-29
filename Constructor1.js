7b9b7b35998f0305e654fd7788fcfb295dd4236d
LakshmiChokkalingam <lakshmi_chokkalingam@discovery.com>
2024-10-29 18:12:51 +0530

    Constructor

---
+++ Constructor1.js
@@ -0,0 +1,17 @@
+class person 
+{
+    age = 25
+constructor (firstname,lastname)
+{
+this.Firstname = firstname
+this.Lastname = lastname
+}
+fullname()
+{
+     return this.Firstname+this.Lastname
+     
+}
+}
+
+let Person = new person ("Tim", "John")
+console.log(Person.fullname())
